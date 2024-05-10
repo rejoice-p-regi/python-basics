7ef5034ca3609f6e971c5d203713e9ab46866e64
rejoice <you@rejoicepregi2000@gmail.com>
2024-05-10 11:53:27 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,17 @@
+x=5
+y=10
+print(x+y)
+x=10
+y=5
+print(x-y)
+x=10
+y=5
+print(x*y)
+x=25
+y=5
+print(x/y)
+
+a=80
+b=50
+if a>b:
+    print("a is grester")
---
+++ futura.py
@@ -0,0 +1,17 @@
+#print("new")
+print(5)
+#print(5)
+"""
+ffhhjgjhmnughjhk
+nmvgcfgfjnm,
+mnvygjn ,
+
+"""
+x=5
+print(x)
+y="apple"
+print(y)
+a,b,c ="orange","apple","grape"
+print(a,b,c)
+e=f=g="orange"
+print(e,f,g)
