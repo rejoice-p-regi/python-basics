722331c02fe44097ee76a68d941b6b0984ee4fa8
rejoice <you@rejoicepregi2000@gmail.com>
2024-05-14 23:08:33 +0530

    fruits are good

---
+++ grape is ready.py
@@ -0,0 +1,25 @@
+x="grape"
+print(x)
+print(type(x))
+print(len(x))
+print(x[3])
+for i in x:
+    print(i)
+y=" python, is, a, great, language "
+print(y)
+print(y[2:5])
+print(y[:4])
+print(y[-4])
+print(y[-5:-3])
+print(y.upper())
+print(y.lower())
+print(y.strip())
+print(y.replace("p","m"))
+print(y.split(","))
+print(x+y)
+print(x+" "+y)
+time=8
+text="mohan can do work in {} days"
+print(text.format(time))
+x="rahul likes to watch football in tv and play football"
+print(x.count("football"))
