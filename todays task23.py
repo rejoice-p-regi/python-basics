49dea7159ce2f6c70bff8b6a7b582bc9a73863da
rejoice <you@rejoicepregi2000@gmail.com>
2024-05-22 13:28:03 +0530

    hello

---
+++ todays task23.py
@@ -0,0 +1,12 @@
+x=int(input("enter first number"))
+if x>0:
+    print("x is postive")
+elif x==0:
+    print("x is equal to 0")
+else:
+    print("x is negative")
+# t=int(input("enter a number"))
+# if t%2==0:
+#     print(t," is a even number")
+# else:
+#     print(t,"odd number")
---
+++ todays work11.py
@@ -0,0 +1,25 @@
+x=1
+y=2
+if x>y:
+    print("x is greater than y")
+else:
+    print("y is greater than x")
+p=52
+r=100
+if p>r:
+    print("p is greater than r")
+elif p==r:
+    print("p is equal to r")
+else:
+    print("p is less than r")
+t=21
+if t>10:
+    print("t is above 10")
+    if t>20:
+        print("t is also above 20")
+    else:
+        print("t is below 20")
+else:
+    print("t is below 10")
+if x>y:
+    pass
