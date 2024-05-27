aee0583c547be01922df13e62a7a22b186ca585d
rejoice <you@rejoicepregi2000@gmail.com>
2024-05-27 11:27:41 +0530

    hello

--- 27-05-2024task04.py
+++ 27-05-2024task04.py
@@ -1,17 +1,17 @@
-# def fibonacci(n):
-#     fib1 = [0,1]
-#     for i in range(2,n):
-#         fib2 = fib1[-1] + fib1[-2]
-#         fib1.append(fib2)
-#     return fib1[:n]
-# n=10
-# print(fibonacci(n))
+def fibonacci(n):
+    fib1 = [0,1]
+    for i in range(2,n):
+        fib2 = fib1[-1] + fib1[-2]
+        fib1.append(fib2)
+    return fib1[:n]
+n=10
+print(fibonacci(n))
 
-# factorial using function
-def factorial(x):
-    if x==0:
-        return 1
-    else:
-        return(x*factorial(x-1))
-x=int(input("enter a value"))
-print(factorial(x))
+factorial using function
+# def factorial(x):
+#     if x==0:
+#         return 1
+#     else:
+#         return(x*factorial(x-1))
+# x=int(input("enter a value"))
+# print(factorial(x))
