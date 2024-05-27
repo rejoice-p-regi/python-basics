401fa4cba1c9bd66280bff23225777afdcd67a36
rejoice <you@rejoicepregi2000@gmail.com>
2024-05-27 11:23:55 +0530

    hello

--- 27-05-2024task03.py
+++ 27-05-2024task03.py
@@ -1,22 +1,7 @@
-rows = int(input("Enter number of rows: "))
-
-k = 0
-count=0
-count1=0
-
-for i in range(1, rows+1):
-    for space in range(1, (rows-i)+1):
-        print("  ", end="")
-        count+=1
-    
-    while k!=((2*i)-1):
-        if count<=rows-1:
-            print(i+k, end=" ")
-            count+=1
-        else:
-            count1+=1
-            print(i+k-(2*count1), end=" ")
-        k += 1
-    
-    count1 = count = k = 0
+a=int(input("Enter number"))
+for i in range(a):
+    for j in range(a-i-1):
+        print("",end=' ')
+    for j in range(i+1):
+        print("*",end=' ')
     print()
